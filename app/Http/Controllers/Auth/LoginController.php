545607564272b2628041f57374e779f2a7eb0ddb
alamin <alamincsetpi@gmail.com>
2024-12-05 12:35:37 +0600

    Changes route name and added new route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,6 +54,6 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect()->route('login');
+        return redirect()->to('/');
     }
 }
